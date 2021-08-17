74e93f5c5485e388619d7cc653a0150aeb203d47
Sarah <sarahleticiasr@gmail.com>
2021-08-17 00:19:16 -0300

    iniciando mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sarah_mvs_entity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
